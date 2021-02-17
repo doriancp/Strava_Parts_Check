222974e5c98fc150c90a91a2d993553defb3f25c
doriancp <75136399+doriancp@users.noreply.github.com>
2021-02-17 16:07:24 +0000

    Update strava_b.py

--- strava_b.py
+++ strava_b.py
@@ -108,24 +108,3 @@ def clean_bike():
 
 
 distance_travelled()
-
-
-
-
-
-# while True:
-#     try:
-#         # Note: Python 2.x users should use raw_input, the equivalent of 3.x's input
-#         age = int(input("Please enter your age: "))
-#     except ValueError:
-#         print("Sorry, I didn't understand that.")
-#         #better try again... Return to the start of the loop
-#         continue
-#     else:
-#         #age was successfully parsed!
-#         #we're ready to exit the loop.
-#         break
-# if age >= 18: 
-#     print("You are able to vote in the United States!")
-# else:
-#     print("You are not able to vote in the United States.")
